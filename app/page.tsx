3829fa5a470edc4388cd9c5fb6203710c4f782f4
Alek30k <alemank23k@gmail.com>
2025-07-10 16:53:57 -0300

    last commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { motion } from "motion/react";
 
 export default function home() {
   return (
-    <div className="relative mx-auto my-10 flex max-w-7xl flex-col items-center justify-center">
+    <div className="relative  my-10 flex  flex-col items-center justify-center">
       <Navbar />
       <div className="absolute inset-y-0 left-0 h-full w-px bg-neutral-200/80 dark:bg-neutral-800/80">
         <div className="absolute top-0 h-40 w-px bg-gradient-to-b from-transparent via-blue-500 to-transparent" />
@@ -17,7 +17,7 @@ export default function home() {
       </div>
       <div className="px-4 py-10 md:py-20">
         <h1 className="relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold text-slate-700 md:text-4xl lg:text-7xl dark:text-slate-300">
-          {"Launch your website in hours, not days"
+          {"🧠 Transforma la atención médica con agentes de voz médicos de IA"
             .split(" ")
             .map((word, index) => (
               <motion.span
@@ -48,9 +48,9 @@ export default function home() {
           }}
           className="relative z-10 mx-auto max-w-xl py-4 text-center text-lg font-normal text-neutral-600 dark:text-neutral-400"
         >
-          With AI, you can launch your website in hours, not days. Try our best
-          in class, state of the art, cutting edge AI tools to get your website
-          up.
+          Ofrezca asistencia médica inteligente 24/7 mediante IA conversacional.
+          Clasifique síntomas, reserve citas y brinde atención empática con
+          automatización basada en la voz.
         </motion.p>
         <motion.div
           initial={{
@@ -66,11 +66,11 @@ export default function home() {
           className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
         >
           <button className="w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
-            Explore Now
+            Empezar
           </button>
-          <button className="w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900">
+          {/* <button className="w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900">
             Contact Support
-          </button>
+          </button> */}
         </motion.div>
         <motion.div
           initial={{
