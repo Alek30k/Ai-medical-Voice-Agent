eb69ceae5b34e2fcb227f7bb1638615be6d9b34a
Alek30k <alemank23k@gmail.com>
2025-07-13 11:55:15 -0300

    last commit

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,8 +1,25 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import axios from "axios";
+import { useEffect } from "react";
+
 const Provider = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  const { user } = useUser();
+
+  useEffect(() => {
+    user && CreateNewUser();
+  }, [user]);
+
+  const CreateNewUser = async () => {
+    const result = await axios.post("/api/users");
+
+    console.log(result.data);
+  };
   return <div className="">{children}</div>;
 };
 
