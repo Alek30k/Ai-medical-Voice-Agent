dbda1be17074537c7484955279de233370b6df54
Alek30k <alemank23k@gmail.com>
2025-07-19 17:16:06 -0300

    last commit

--- app/(routes)/dashboard/_components/HistoryList.tsx
+++ app/(routes)/dashboard/_components/HistoryList.tsx
@@ -1,5 +1,27 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
 const HistoryList = () => {
-  return <div className="">HistoryList</div>;
+  const [historyList, setHistoryList] = useState([]);
+
+  return (
+    <div className="">
+      {historyList.length == 0 ? (
+        <div className="">
+          <Image
+            src={"/medical-assistance.png"}
+            alt="Vacio"
+            width={200}
+            height={200}
+          />
+        </div>
+      ) : (
+        <div className="">List</div>
+      )}
+    </div>
+  );
 };
 
 export default HistoryList;
--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -4,7 +4,7 @@ import HistoryList from "./_components/HistoryList";
 const Dashboard = () => {
   return (
     <div className="">
-      <div className="">
+      <div className="flex justify-between items-center ">
         <h2 className="font-bold text-2xl">My Dashboard</h2>
         <Button>+ Consultar con el doctor</Button>
       </div>
